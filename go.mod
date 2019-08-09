e3dd30890b887effa91fffb80c500a74808b8c04
Hariharan Srinath <srinathh@gmail.com>
2019-08-09 22:22:09 +0800

    Step 5: Turn off echo

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/srinathh/gokilo
 
 go 1.12
+
+require golang.org/x/sys v0.0.0-20190804053845-51ab0e2deafa
--- gokilo.go
+++ gokilo.go
@@ -3,16 +3,42 @@ package main
 import (
 	"os"
 	"fmt"
+	syscall "golang.org/x/sys/unix"
 )
 
-func main(){
-	b := []byte{0}
+func enableRawMode() error {
 
+	termios, err := syscall.IoctlGetTermios(syscall.Stdin, syscall.TCGETS)
+	if err != nil{
+		return err
+	} 
+
+	termios.Lflag = termios.Lflag &^syscall.ECHO
+
+	if err := syscall.IoctlSetTermios(syscall.Stdin, syscall.TCSETS, termios); err != nil{
+		return err
+	}
+
+	return nil
+}
+
+func main(){
+
+	if err := enableRawMode(); err != nil{
+		fmt.Fprintln(os.Stderr, err)
+	}
+	
+
+	b := []byte{0}
 	for{
 		_, err := os.Stdin.Read(b)		
 		if err != nil{
 			fmt.Fprintf(os.Stderr,"Error: %s\n", err)
 			os.Exit(1)
+		}
+
+		if b[0]=='q'{
+			break
 		}
 	}
 }
