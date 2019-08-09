bcffe2904bb5117d50564296f41b51da009e9e31
Hariharan Srinath <srinathh@gmail.com>
2019-08-09 21:06:33 +0800

    Step 2:  getting user input

--- gokilo.go
+++ gokilo.go
@@ -1,5 +1,18 @@
 package main 
 
-func main(){
+import (
+	"os"
+	"fmt"
+)
 
+func main(){
+	b := []byte{0}
+
+	for{
+		_, err := os.Stdin.Read(b)		
+		if err != nil{
+			fmt.Fprintf(os.Stderr,"Error: %s\n", err)
+			os.Exit(1)
+		}
+	}
 }
