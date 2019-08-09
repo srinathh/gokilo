3b75d3a5495fbde7800687a373102ccc11a5c215
Hariharan Srinath <srinathh@gmail.com>
2019-08-09 23:31:44 +0800

    Step 10: Disable Ctrl+S and Ctrl+Q capture

--- gokilo.go
+++ gokilo.go
@@ -27,6 +27,7 @@ func enableRawMode() error {
 
 	// turn off echo & canonical mode by using a bitwise clear operator &^
 	termios.Lflag = termios.Lflag &^ (syscall.ECHO|syscall.ICANON|syscall.ISIG)
+	termios.Iflag = termios.Iflag &^ (syscall.IXON)
 
 	// We from the code of tcsetattr in glibc, we find that for TCSAFLUSH, 
 	// the corresponding command is TCSETSF 
