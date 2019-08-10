5ce4e01c7cd824196d032564299e141b45a3fb8a
Hariharan Srinath <srinathh@gmail.com>
2019-08-10 22:11:25 +0800

    Step 42: Center horizontally + bug fix truncation If the message is too long, we truncate to screen cols. THis was erroneous previously

--- gokilo.go
+++ gokilo.go
@@ -150,11 +150,23 @@ func editorDrawRows(ab *bytes.Buffer){
 			welcomeMsg := fmt.Sprintf("Kilo Editor -- version %s", kiloVersion)
 			welcomeLen := len(welcomeMsg)
 
+			// if the message is too long to fit, truncate
 			if welcomeLen > cfg.screenCols{
-				welcomeMsg = welcomeMsg[:welcomeLen]
+				welcomeMsg = welcomeMsg[:cfg.screenCols]
 				welcomeLen= cfg.screenCols
 			}
+			padding := (cfg.screenCols - welcomeLen)/2
 
+			// if there is at least 1 padding required, use the Tilde to start line
+			if padding > 0{
+				fmt.Fprint(ab, "~")
+				padding--
+			}
+			
+			// add appropriate number of spaces
+			for i := 0; i < padding; i++{
+				fmt.Fprint(ab, " ")
+			}
 			fmt.Fprint(ab, welcomeMsg)
 
 		}else{
