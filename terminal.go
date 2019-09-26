b71cdd023a87acb74ab919a74ccd00a324d42b51
Hariharan Srinath <srinathh@gmail.com>
2019-09-26 22:29:59 +0800

    moved platform specific termios to separte file with build tag

--- terminal.go
+++ terminal.go
@@ -5,46 +5,7 @@ import (
 	"fmt"
 	"io"
 	"os"
-
-	syscall "golang.org/x/sys/unix"
 )
-
-// enableRawMode switches from cooked or canonical mode to raw mode
-// by using syscalls. Currently this is the implrementation for Unix only
-func enableRawMode() error {
-
-	// Gets TermIOS data structure. From glibc, we find the cmd should be TCGETS
-	// https://code.woboq.org/userspace/glibc/sysdeps/unix/sysv/linux/tcgetattr.c.html
-	termios, err := syscall.IoctlGetTermios(syscall.Stdin, syscall.TCGETS)
-	if err != nil {
-		return err
-	}
-
-	cfg.origTermios = *termios
-
-	// turn off echo & canonical mode by using a bitwise clear operator &^
-	termios.Lflag = termios.Lflag &^ (syscall.ECHO | syscall.ICANON | syscall.ISIG | syscall.IEXTEN)
-	termios.Iflag = termios.Iflag &^ (syscall.IXON | syscall.ICRNL | syscall.BRKINT | syscall.INPCK | syscall.ISTRIP)
-	termios.Oflag = termios.Oflag &^ (syscall.OPOST)
-	termios.Cflag = termios.Cflag | syscall.CS8
-	termios.Cc[syscall.VMIN] = 0
-	termios.Cc[syscall.VTIME] = 1
-	// from the code of tcsetattr in glibc, we find that for TCSAFLUSH,
-	// the corresponding command is TCSETSF
-	// https://code.woboq.org/userspace/glibc/sysdeps/unix/sysv/linux/tcsetattr.c.html
-	if err := syscall.IoctlSetTermios(syscall.Stdin, syscall.TCSETSF, termios); err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func disableRawMode() error {
-	if err := syscall.IoctlSetTermios(syscall.Stdin, syscall.TCSETSF, &cfg.origTermios); err != nil {
-		return err
-	}
-	return nil
-}
 
 func safeExit(err error) {
 	fmt.Fprint(os.Stdout, "\x1b[2J")
@@ -158,18 +119,4 @@ func editorReadKey() int {
 			return int(key)
 		}
 	}
-}
-
-func getWindowSize() (int, int, error) {
-
-	ws, err := syscall.IoctlGetWinsize(syscall.Stdout, syscall.TIOCGWINSZ)
-	if err != nil {
-		return 0, 0, err
-	}
-	if ws.Row == 0 || ws.Col == 0 {
-		return 0, 0, errors.New("got non zero column or row")
-	}
-
-	return int(ws.Row), int(ws.Col), nil
-
 }
---
+++ termios_unix.go
@@ -0,0 +1,60 @@
+// +build !windows
+
+package main
+
+import (
+	"errors"
+
+	"golang.org/x/sys/unix"
+)
+
+func getWindowSize() (int, int, error) {
+
+	ws, err := unix.IoctlGetWinsize(unix.Stdout, unix.TIOCGWINSZ)
+	if err != nil {
+		return 0, 0, err
+	}
+	if ws.Row == 0 || ws.Col == 0 {
+		return 0, 0, errors.New("got non zero column or row")
+	}
+
+	return int(ws.Row), int(ws.Col), nil
+
+}
+
+// enableRawMode switches from cooked or canonical mode to raw mode
+// by using syscalls. Currently this is the implrementation for Unix only
+func enableRawMode() error {
+
+	// Gets TermIOS data structure. From glibc, we find the cmd should be TCGETS
+	// https://code.woboq.org/userspace/glibc/sysdeps/unix/sysv/linux/tcgetattr.c.html
+	termios, err := unix.IoctlGetTermios(unix.Stdin, unix.TCGETS)
+	if err != nil {
+		return err
+	}
+
+	cfg.origTermios = *termios
+
+	// turn off echo & canonical mode by using a bitwise clear operator &^
+	termios.Lflag = termios.Lflag &^ (unix.ECHO | unix.ICANON | unix.ISIG | unix.IEXTEN)
+	termios.Iflag = termios.Iflag &^ (unix.IXON | unix.ICRNL | unix.BRKINT | unix.INPCK | unix.ISTRIP)
+	termios.Oflag = termios.Oflag &^ (unix.OPOST)
+	termios.Cflag = termios.Cflag | unix.CS8
+	termios.Cc[unix.VMIN] = 0
+	termios.Cc[unix.VTIME] = 1
+	// from the code of tcsetattr in glibc, we find that for TCSAFLUSH,
+	// the corresponding command is TCSETSF
+	// https://code.woboq.org/userspace/glibc/sysdeps/unix/sysv/linux/tcsetattr.c.html
+	if err := unix.IoctlSetTermios(unix.Stdin, unix.TCSETSF, termios); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func disableRawMode() error {
+	if err := unix.IoctlSetTermios(unix.Stdin, unix.TCSETSF, &cfg.origTermios); err != nil {
+		return err
+	}
+	return nil
+}
