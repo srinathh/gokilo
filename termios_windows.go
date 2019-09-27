8fd694f642710eb19db33f4beee9141260e59fdd
Hariharan Srinath <srinathh@gmail.com>
2019-09-27 16:08:38 +0800

    basic windows function working but something in editorDrawRows incorrect

--- termios_windows.go
+++ termios_windows.go
@@ -19,3 +19,23 @@ func getWindowSize() (int, int, error) {
 	return int(info.Size.Y), int(info.Size.X), nil
 
 }
+
+func enableRawMode() error {
+
+	var inSettings uint32 = windows.ENABLE_EXTENDED_FLAGS | windows.ENABLE_VIRTUAL_TERMINAL_INPUT
+	var outSettings uint32 = windows.ENABLE_VIRTUAL_TERMINAL_PROCESSING | windows.ENABLE_PROCESSED_OUTPUT
+
+	if err := windows.SetConsoleMode(windows.Stdin, inSettings); err != nil {
+		return fmt.Errorf("error setting Raw mode: %s", err)
+	}
+
+	if err := windows.SetConsoleMode(windows.Stdout, outSettings); err != nil {
+		return fmt.Errorf("error setting output VT100: %s", err)
+	}
+
+	return nil
+}
+
+func disableRawMode() error {
+	return nil
+}
