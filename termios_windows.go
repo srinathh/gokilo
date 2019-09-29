aee20a7137d444109c76d0fdc60e1148c0db2849
Hariharan Srinath <srinathh@gmail.com>
2019-09-29 14:38:23 +0800

    disable raw mode added to windows termios

--- termios_windows.go
+++ termios_windows.go
@@ -8,6 +8,11 @@ import (
 	"golang.org/x/sys/windows"
 )
 
+type winConsoleSettings struct {
+	In  uint32
+	Out uint32
+}
+
 func getWindowSize() (int, int, error) {
 
 	info := windows.ConsoleScreenBufferInfo{}
@@ -22,8 +27,19 @@ func getWindowSize() (int, int, error) {
 
 func enableRawMode() error {
 
+	oldSettings := winConsoleSettings{}
+
+	if err := windows.GetConsoleMode(windows.Stdin, &oldSettings.In); err != nil {
+		return fmt.Errorf("error gettingRaw mode: %s", err)
+	}
+	if err := windows.GetConsoleMode(windows.Stdout, &oldSettings.Out); err != nil {
+		return fmt.Errorf("error gettingRaw mode: %s", err)
+	}
+
+	cfg.origTermCfg = oldSettings
+
 	var inSettings uint32 = windows.ENABLE_EXTENDED_FLAGS | windows.ENABLE_VIRTUAL_TERMINAL_INPUT
-	var outSettings uint32 = windows.ENABLE_VIRTUAL_TERMINAL_PROCESSING | windows.ENABLE_PROCESSED_OUTPUT| windows.DISABLE_NEWLINE_AUTO_RETURN
+	var outSettings uint32 = windows.ENABLE_VIRTUAL_TERMINAL_PROCESSING | windows.ENABLE_PROCESSED_OUTPUT | windows.DISABLE_NEWLINE_AUTO_RETURN
 
 	if err := windows.SetConsoleMode(windows.Stdin, inSettings); err != nil {
 		return fmt.Errorf("error setting Raw mode: %s", err)
@@ -37,5 +53,13 @@ func enableRawMode() error {
 }
 
 func disableRawMode() error {
+	oldSettings := cfg.origTermCfg.(winConsoleSettings)
+	if err := windows.SetConsoleMode(windows.Stdin, oldSettings.In); err != nil {
+		return fmt.Errorf("error setting Raw mode: %s", err)
+	}
+
+	if err := windows.SetConsoleMode(windows.Stdout, oldSettings.Out); err != nil {
+		return fmt.Errorf("error setting output VT100: %s", err)
+	}
 	return nil
 }
