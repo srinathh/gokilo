cfeaa616ed016a046a8d01b843ac2080fe37ec8c
Hariharan Srinath <srinathh@gmail.com>
2020-02-09 22:30:44 +0800

    cleaned up main & moved key handling to dispatch

--- gokilo.go
+++ gokilo.go
@@ -33,6 +33,11 @@ var SafeExit func(error)
 
 func main() {
 
+	// parse config flags & parameters
+	flag.Parse()
+	filename := flag.Arg(0)
+
+	// enable raw mode
 	origCfg, err := rawmode.Enable()
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Error enabling raw mode: %v", err)
@@ -41,19 +46,15 @@ func main() {
 
 	SafeExit = func(error) { safeExit(origCfg, err) }
 
+	// get the screen dimensions and create a view
 	rows, cols, err := rawmode.GetWindowSize()
 	if err != nil {
 		SafeExit(fmt.Errorf("couldn't get window size: %v", err))
 	}
+	v := NewView(rows, cols)
 
-	view := NewView(rows, cols)
-
+	// create the editor
 	var e *Editor
-	//var s *Session
-
-	flag.Parse()
-	filename := flag.Arg(0)
-
 	if flag.Arg(0) == "" {
 		e = NewEditor()
 	} else {
@@ -65,108 +66,59 @@ func main() {
 	//s = NewSession(filename)
 
 	for {
-		view.RefreshScreen(e)
+		// redraw the screen
+		v.RefreshScreen(e)
 
+		// read key
 		k, err := terminal.ReadKey()
 		if err != nil {
 			SafeExit(fmt.Errorf("Error reading from terminal: %s", err))
 		}
 
-		if k.Special == terminal.KeyNoSpl {
-			switch k.Regular {
-			case '\r':
-			//	session.Editor.InsertNewline()
+		// dispatch the key
+		dispatchKey(k, nil, v, e)
 
-			case ctrlKey('l'), '\x1b':
-				break
+	}
+}
 
-			case ctrlKey('q'):
-				//session.Quit()
-				SafeExit(nil)
+func dispatchKey(k terminal.Key, s *Session, v *View, e *Editor) {
 
-			case ctrlKey('s'):
-				//session.Save()
+	if k.Special == terminal.KeyNoSpl {
+		switch k.Regular {
+		case '\r':
+		//	session.Editor.InsertNewline()
 
-			case ctrlKey('f'):
-				//editorFind()
+		case ctrlKey('l'), '\x1b':
+			break
 
-			case ctrlKey('h'), 127:
-				//session.Editor.DelChar()
+		case ctrlKey('q'):
+			//session.Quit()
+			SafeExit(nil)
 
-			default:
-				//session.Editor.InsertChar(k.Regular)
-			}
-		} else {
-			switch k.Special {
+		case ctrlKey('s'):
+			//session.Save()
 
-			case terminal.KeyArrowDown, terminal.KeyArrowLeft, terminal.KeyArrowRight, terminal.KeyArrowUp:
-				//session.Editor.MoveCursor(k.Special)
+		case ctrlKey('f'):
+			//editorFind()
 
-			case terminal.KeyPageUp, terminal.KeyPageDown, terminal.KeyHome, terminal.KeyEnd:
-				///session.Editor.MoveScreen(k.Special)
+		case ctrlKey('h'), 127:
+			//session.Editor.DelChar()
 
-			case terminal.KeyDelete:
-				//session.Editor.MoveCursor(terminal.KeyArrowRight)
-				//session.Editor.DelChar()
-			}
+		default:
+			//session.Editor.InsertChar(k.Regular)
+		}
+	} else {
+		switch k.Special {
+
+		case terminal.KeyArrowDown, terminal.KeyArrowLeft, terminal.KeyArrowRight, terminal.KeyArrowUp:
+			//session.Editor.MoveCursor(k.Special)
+
+		case terminal.KeyPageUp, terminal.KeyPageDown, terminal.KeyHome, terminal.KeyEnd:
+			///session.Editor.MoveScreen(k.Special)
+
+		case terminal.KeyDelete:
+			//session.Editor.MoveCursor(terminal.KeyArrowRight)
+			//session.Editor.DelChar()
 		}
 	}
 }
-
-/*
-func initEditor() error {
-	cfg.ScreenRows = rows
-	cfg.ScreenRows = cfg.ScreenRows - 2
-	cfg.ScreenCols = cols
-	editor.QuitTimes = kiloQuitTimes
-	editor.LastMatch = -1
-	editor.Direction = 1
-	editorSetStatusMsg("HELP: Ctrl + S to save | Ctrl + Q to exit | Ctrl + F to find")
-	return nil
-}
-
-func main() {
-
-	b, err := rawmode.Enable()
-	if err != nil {
-		safeExit(err)
-	}
-	cfg.OrigTermCfg = b
-
-	if err := initEditor(); err != nil {
-		safeExit(err)
-	}
-
-	if len(os.Args) == 2 {
-		rows, err := Open(os.Args[1])
-		if err != nil {
-			safeExit(err)
-		}
-		editor.Rows = rows
-		editor.FileName = os.Args[1]
-	}
-
-	for {
-		editorRefreshScreen()
-		if err := editorProcessKeypress(); err != nil {
-			safeExit(err)
-		}
-	}
-}
-
-func safeExit(err error) {
-	fmt.Fprint(os.Stdout, "\x1b[2J")
-	fmt.Fprint(os.Stdout, "\x1b[H")
-
-	if err1 := rawmode.Restore(cfg.OrigTermCfg); err1 != nil {
-		fmt.Fprintf(os.Stderr, "Error: disabling raw mode: %s\r\n", err)
-	}
-
-	if err == nil {
-		os.Exit(0)
-	}
-
-	fmt.Fprintf(os.Stderr, "Error: %s\r\n", err)
-	os.Exit(1)
-}
-*/
